01ee4980cbb7fc9df04951580014d6aee6c11cc3
Roberto Hernandez <robertotik2001@gmail.com>
2024-02-28 22:35:05 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import "es6-shim";
 import express, { Express, Request, Response } from "express";
 import { Pool } from "pg";
 import "reflect-metadata";
-import { Board } from "./dto/board.dto";
-import { User } from "./dto/user.dto";
+import { Board } from "./dto/Board";
+import { List } from "./dto/List";
+import { Card } from "./dto/Card";
+import { User } from "./dto/User";
 
 dotenv.config();
 
@@ -18,14 +20,16 @@ const pool = new Pool({
   port: +process.env.DB_PORT!,
 });
 
+
+// Definir una instancia de la aplicación Express
 const app: Express = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/users", async (req: Request, res: Response) => {
   try {
-    const text = "SELECT id, name, email FROM users";
-    const result = await pool.query(text);
+    const query = 'SELECT id, name, email FROM "user"';
+    const result = await pool.query(query); //Hace la consulta a la base de datos y se almacena en result.
     res.status(200).json(result.rows);
   } catch (errors) {
     return res.status(400).json(errors);
@@ -33,60 +37,180 @@ app.get("/users", async (req: Request, res: Response) => {
 });
 
 app.post("/users", async (req: Request, res: Response) => {
-  let userDto: User = plainToClass(User, req.body);
+  let userDto: User = plainToClass(User, req.body); // Decorador
   try {
-    await validateOrReject(userDto);
+    await validateOrReject(userDto); // Decorador
 
-    const text = "INSERT INTO users(name, email) VALUES($1, $2) RETURNING *";
-    const values = [userDto.name, userDto.email];
-    const result = await pool.query(text, values);
-    res.status(201).json(result.rows[0]);
+    const query = 'INSERT INTO "user"(name, email) VALUES($1, $2) RETURNING *'; //Guarda la peticion en text 
+    const values = [userDto.name, userDto.email]; //Almacena los datos de name y email del JSON para luego ser enviado a la base de datos
+    const result = await pool.query(query, values); // Implementa la accion almacenado en text con los datos almacenado en values
+    res.status(201).json(result.rows[0]); //Todo funco correctamente :)
   } catch (errors) {
-    return res.status(422).json(errors);
+    return res.status(422).json(errors); //No funco como deberia :(
+  }
+}); //Funca 
+
+// Crear tablero de usuario (DML - INSERT)
+app.post('/boards/:userId', async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  let newBoard: Board = plainToClass(Board, req.body); // Decorador
+
+  const resp = await pool.connect();
+  try {
+    await validateOrReject(newBoard); // Decorador
+
+    resp.query("BEGIN");
+    const query = 'INSERT INTO "board" (name) VALUES ($1) RETURNING *'; // DML - INSERT
+    const values = [newBoard.name]; // Extrae el nombre del formato JSON para insertarlo en la query
+    const { rows } = await resp.query(query, values); // DML - Query Execution
+
+    const BoardUserId = rows[0].id; //Extrae el id generado por el INSERT de board.
+    const assignQuery = 'INSERT INTO "boarduser" (board_id, user_id, is_admin) VALUES ($1, $2, true)'; // DML - INSERT
+    await resp.query(assignQuery, [BoardUserId, userId]); // DML - Query Execution
+
+    resp.query("COMMIT");
+    res.json({ board: rows[0] });
+  } catch (error) {
+    resp.query("ROLLBACK");
+    res.status(500).json({ error: 'Ocurrió un error al crear el tablero' });
+  }
+}); //Funca
+
+// Asignar un usuario a un tablero (DML - INSERT)
+app.post('/boards/:boardId/users', async (req: Request, res: Response) => {
+  const boardId = req.params.boardId; // Extrae el boardId del link
+  const userId = req.body.userId; // Extrae el userId del formato JSON
+
+  const resp = await pool.connect();
+  try {
+    resp.query("BEGIN");
+    const query = 'INSERT INTO "boarduser" (board_id, user_id) VALUES ($1, $2)'; // DML - INSERT
+    await resp.query(query, [boardId, userId]); // DML - Query Execution
+    // await indica que la ejecución del código se pausará hasta que la consulta se complete y se obtenga una respuesta de la base de datos.
+
+    resp.query("COMMIT");
+    res.json({ message: 'Usuario asignado al tablero' });
+  } catch (error) {
+    resp.query("ROLLBACK");
+    res.status(500).json({ error: 'Ocurrió un error al asignar el usuario a al tablero' });
+  }
+});//Funca
+
+
+// Obtener las listas de un tablero específico (DQL - SELECT)
+app.get('/boards/:boardId/lists', async (req: Request, res: Response) => {
+  const boardId = req.params.boardId;
+
+  try {
+    const query = 'SELECT * FROM "list" WHERE board_id = $1'; // DQL - SELECT
+    const { rows } = await pool.query(query, [boardId]); // DML - Query Execution
+    res.json({ lists: rows });
+  } catch (error) {
+    res.status(500).json({ error: 'Ocurrió un error al obtener las listas' });
+  }
+}); //Funca
+
+// Crear una lista en un tablero (DML - INSERT)
+app.post('/boards/:boardId/lists', async (req: Request, res: Response) => {
+  const boardId = req.params.boardId;
+  let newList: List = plainToClass(List, req.body); // Decorador
+  
+
+  const resp = await pool.connect();
+  try {
+    await validateOrReject(newList); // Decorador
+
+    resp.query("BEGIN");
+    const query = 'INSERT INTO "list" (name, board_id) VALUES ($1, $2) RETURNING *'; // DML - INSERT
+    const values = [newList.name, boardId];
+    const { rows } = await resp.query(query, values); // DML - Query Execution
+    resp.query("COMMIT");
+    res.json({ list: rows[0] });
+  } catch (error) {
+    resp.query("ROLLBACK");
+    res.status(500).json({ error: 'Ocurrió un error al crear la lista' });
+  }
+}); //Funca
+
+// Crear una tarjeta en una lista (DML - INSERT)
+app.post('/lists/:listId/users/:userId/cards', async (req: Request, res: Response) => {
+  const listId = req.params.listId;
+  const userId = req.params.userId;
+  let newCard: Card = plainToClass(Card, req.body); // Decorador
+    
+
+  const resp = await pool.connect();
+  try {
+    await validateOrReject(newCard); // Decorador
+
+    resp.query("BEGIN");
+    const query = 'INSERT INTO "card" (title, description, due_date, list_id) VALUES ($1, $2, $3, $4) RETURNING *'; // DML - INSERT
+    const values = [newCard.title, newCard.description, newCard.due_date, listId];
+    const { rows } = await resp.query(query, values); // DML - Query Execution
+
+    const cardId = rows[0].id; //Extrae el id generado por el INSERT de Card.
+    const assignQuery = 'INSERT INTO "carduser" (card_id, user_id, is_owner) VALUES ($1, $2, true)'; // DML - INSERT
+    await resp.query(assignQuery, [cardId, userId]); // DML - Query Execution
+
+    resp.query("COMMIT");
+    res.json({ card: rows[0] });
+  } catch (error) {
+    resp.query("ROLLBACK");
+    res.status(500).json({ error: 'Ocurrió un error al crear la tarjeta' });
+  }
+}); //Funca
+
+// Asignar un usuario a una tarjeta (DML - INSERT)
+app.post('/cards/:cardId/users', async (req: Request, res: Response) => {
+  const cardId = req.params.cardId;
+  const userId = req.body.userId;
+
+  const resp = await pool.connect();
+  try {
+    resp.query("BEGIN");
+    const query = 'INSERT INTO "carduser" (card_id, user_id) VALUES ($1, $2)'; // DML - INSERT
+    await resp.query(query, [cardId, userId]); // DML - Query Execution
+
+    resp.query("COMMIT");
+    res.json({ message: 'Usuario asignado a la tarjeta' });
+  } catch (error) {
+    resp.query("ROLLBACK");
+    res.status(500).json({ error: 'Ocurrió un error al asignar el usuario a la tarjeta' });
   }
 });
 
-app.get("/boards", async (req: Request, res: Response) => {
-  try {
-    const text =
-      'SELECT b.id, b.name, bu.userId "adminUserId" FROM boards b JOIN board_users bu ON bu.boardId = b.id WHERE bu.isAdmin IS true';
-    const result = await pool.query(text);
-    res.status(200).json(result.rows);
-  } catch (errors) {
-    return res.status(400).json(errors);
-  }
-});
+  // Obtener una tarjeta con el user que la creó.
+  app.get("/cards/:listId", async (req: Request, res: Response) => {
+    const list_Id = req.params.listId;
+    try {
+      const Consulta1 = 'SELECT id FROM "card" WHERE list_id = $1';
+      const dato1 = [list_Id];
+      const resultado1 = await pool.query(Consulta1, dato1);
+      const cardId = resultado1.rows[0].id;
+  
+      const Consulta2 = `
+        SELECT User.name 
+        FROM carduser CardUser
+        JOIN "user" User ON CardUser.user_id = User.id
+        WHERE CardUser.card_id = $1 AND CardUser.is_owner = true
+      `;
+      const dato2 = [cardId];
+      const resultado2 = await pool.query(Consulta2, dato2);
+  
+      const response = {
+        cards: resultado1.rows,
+        ownerName: resultado2.rows[0].name
+      };
+  
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(error);
+    }
+  }); //Funca
 
-app.post("/boards", async (req: Request, res: Response) => {
-  let boardDto: Board = plainToClass(Board, req.body);
-  const client = await pool.connect();
-  try {
-    client.query("BEGIN");
-    await validateOrReject(boardDto, {});
 
-    const boardText = "INSERT INTO boards(name) VALUES($1) RETURNING *";
-    const boardValues = [boardDto.name];
-    const boardResult = await client.query(boardText, boardValues);
-
-    const boardUserText =
-      "INSERT INTO board_users(boardId, userId, isAdmin) VALUES($1, $2, $3)";
-    const boardUserValues = [
-      boardResult.rows[0].id,
-      boardDto.adminUserId,
-      true,
-    ];
-    await client.query(boardUserText, boardUserValues);
-
-    client.query("COMMIT");
-    res.status(201).json(boardResult.rows[0]);
-  } catch (errors) {
-    client.query("ROLLBACK");
-    return res.status(422).json(errors);
-  } finally {
-    client.release();
-  }
-});
-
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
